d3c2f96ddca60e4ccb1f9b8020abe3917a29a079
cjparsonson <cjparsonson@gmail.com>
2024-06-05 12:02:13 +0100

    Update - Generate Db and Seed data

--- DigitalBulletJournal/Bala.BulletJournal/Program.cs
+++ DigitalBulletJournal/Bala.BulletJournal/Program.cs
@@ -1,44 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore; // DbContext, DbSet<T>
+using Bala.Shared; // JournalEntry
 
-namespace Bala.Shared;
-
-public class JournalDbContext : DbContext
+using (JournalDbContext db = new())
 {
-    public DbSet<JournalEntry> JournalEntries { get; set; }
+    bool deleted = await db.Database.EnsureDeletedAsync();
+    WriteLine($"Database deleted: {deleted}");
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    bool created = await db.Database.EnsureCreatedAsync();
+    WriteLine($"Database created: {created}");
+
+    WriteLine("SQL script used to create the database:");
+    WriteLine(db.Database.GenerateCreateScript());
+
+    foreach (JournalEntry entry in db.JournalEntries)
     {
-        string path = Path.Combine(
-           Environment.CurrentDirectory, "Journal.db");
-
-        string connection = $"Filename={path}";
-
-        WriteLine($"Connection string: {connection}");
-
-        optionsBuilder.UseSqlite(connection);
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<JournalEntry>(entity =>
-        {
-            entity.HasIndex(e => e.Date);
-            entity.HasIndex(e => e.Rating);
-        });
-
-        modelBuilder.Entity<JournalEntry>().HasData(
-            new JournalEntry
-            {
-                Date = DateTime.Now,
-                Rating = 5,
-                Comment = "Hello World! This is my first journal entry!"
-            },
-            new JournalEntry
-            {
-                Date = DateTime.Now.AddDays(-1),
-                Rating = 4,
-                Comment = "This is my second journal entry! Yesterday was great."
-            }
-        );
+        WriteLine($"Journal entry: {entry.Date:d} {entry.Rating} {entry.Comment}");
     }
 }
+
+
--- DigitalBulletJournal/Bala.DataContext.Sqlite/JournalDbContext.cs
+++ DigitalBulletJournal/Bala.DataContext.Sqlite/JournalDbContext.cs
@@ -1,22 +1,46 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-namespace Bala.Shared
+namespace Bala.Shared;
 
+public class JournalDbContext : DbContext
 {
-    public class JournalDbContext : DbContext
+    public DbSet<JournalEntry> JournalEntries { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        public DbSet<JournalEntry> JournalEntries { get; set; } = default!;
-        
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        string path = Path.Combine(
+           Environment.CurrentDirectory, "Journal.db");
+
+        string connection = $"Filename={path}";
+
+        WriteLine($"Connection string: {connection}");
+
+        optionsBuilder.UseSqlite(connection);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<JournalEntry>(entity =>
         {
-            string path = Path.Combine(
-                Environment.CurrentDirectory, "Journal.db");
+            entity.HasIndex(e => e.Date);
+            entity.HasIndex(e => e.Rating);
+        });
 
-            string connection = $"Filename={path}";
-
-            WriteLine($"Connection: {connection}");
-
-            optionsBuilder.UseSqlite(connection);
-        }
+        modelBuilder.Entity<JournalEntry>().HasData(
+            new JournalEntry
+            {
+                Id = 1,
+                Date = DateTime.Now,
+                Rating = 5,
+                Comment = "Hello World! This is my first journal entry!"
+            },
+            new JournalEntry
+            {
+                Id = 2,
+                Date = DateTime.Now.AddDays(-1),
+                Rating = 4,
+                Comment = "This is my second journal entry! Yesterday was great."
+            }
+        );
     }
 }
