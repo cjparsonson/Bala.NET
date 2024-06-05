435258d48054a4da9fc51b1fe0e96d486c4892bb
cjparsonson <cjparsonson@gmail.com>
2024-06-05 11:33:10 +0100

    Tidy project. Create - Generate Db and sample data.

--- DigitalBulletJournal/Bala.BulletJournal/Program.cs
+++ DigitalBulletJournal/Bala.BulletJournal/Program.cs
@@ -1,3 +1,44 @@
-﻿using Bala.Shared;
+﻿using Microsoft.EntityFrameworkCore; // DbContext, DbSet<T>
 
-WriteLine("Hello World!");
+namespace Bala.Shared;
+
+public class JournalDbContext : DbContext
+{
+    public DbSet<JournalEntry> JournalEntries { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string path = Path.Combine(
+           Environment.CurrentDirectory, "Journal.db");
+
+        string connection = $"Filename={path}";
+
+        WriteLine($"Connection string: {connection}");
+
+        optionsBuilder.UseSqlite(connection);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<JournalEntry>(entity =>
+        {
+            entity.HasIndex(e => e.Date);
+            entity.HasIndex(e => e.Rating);
+        });
+
+        modelBuilder.Entity<JournalEntry>().HasData(
+            new JournalEntry
+            {
+                Date = DateTime.Now,
+                Rating = 5,
+                Comment = "Hello World! This is my first journal entry!"
+            },
+            new JournalEntry
+            {
+                Date = DateTime.Now.AddDays(-1),
+                Rating = 4,
+                Comment = "This is my second journal entry! Yesterday was great."
+            }
+        );
+    }
+}
--- DigitalBulletJournal/Bala.EntityModels.Sqlite/JournalEntry.cs
+++ DigitalBulletJournal/Bala.EntityModels.Sqlite/JournalEntry.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bala.Shared;
 
 public class JournalEntry
-{   
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     [Required]
@@ -12,8 +14,9 @@ public class JournalEntry
 
     [Required]
     [StringLength(200)]
-    public string? Commend { get; set; } = string.Empty; // Default value
+    public string? Comment { get; set; } = string.Empty; // Default value
 
     [Required]
+    [Range(1, 5)]
     public int? Rating { get; set; } = 3; // Default value
 }
