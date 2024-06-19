80b7435ac289436318c45bbeb41601b2235ea931
cjparsonson <cjparsonson@gmail.com>
2024-06-19 21:50:31 +0100

    Update - Create Bala.Services class library and JournalService class.

---
+++ DigitalBulletJournal/Bala.Services/JournalService.cs
@@ -0,0 +1,38 @@
+﻿using Bala.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bala.Services
+{
+    public class JournalService
+    {
+        private readonly JournalDbContext _db;
+        public JournalService(JournalDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<List<JournalEntry>> GetJournalEntriesAsync()
+        {
+            return await _db.JournalEntries.ToListAsync();
+        }
+
+        public async Task AddJournalEntryAsync(JournalEntry entry)
+        {
+            _db.JournalEntries.Add(entry);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task EditJournalEntryAsync(JournalEntry entry)
+        {
+            _db.JournalEntries.Update(entry);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task DeleteJournalEntryAsync(JournalEntry entry)
+        {
+            _db.JournalEntries.Remove(entry);
+            await _db.SaveChangesAsync();
+        }
+
+    }
+}
