cd85aa8030bc7e136aef072d155fff1743ea4f05
cjparsonson <cjparsonson@gmail.com>
2025-03-25 22:08:25 +0000

    Adding components

--- DigitalBulletJournal/Bala.Web/Program.cs
+++ DigitalBulletJournal/Bala.Web/Program.cs
@@ -1,6 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Bala.Web.Components;
 
-app.MapGet("/", () => "Hello World!");
+#region Configue the web server host and services
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorComponents();
+var app = builder.Build();
+#endregion
+
+#region Configure the HTTP request pipeline and routes
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+app.MapRazorComponents<App>();
+app.MapGet("/env", () => 
+    $"Environment is {app.Environment.EnvironmentName}");
+#endregion
 
 app.Run();
